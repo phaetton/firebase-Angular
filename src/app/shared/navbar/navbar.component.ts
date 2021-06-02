628fe91394f20afe8505dfbb68433fd3c5fce74f
phaetton <31552767+phaetton@users.noreply.github.com>
2021-06-02 10:59:34 -0600

    verificando el login

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [AuthService]
 })
 export class NavbarComponent implements OnInit {
+public isLogget=false;
+  constructor(private authSvc: AuthService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit() {
+    console.log('navbar');
+    const user = await this.authSvc.getCurrentUser();
+    if (user) {
+      this.isLogget=true;
+      console.log('user->', user);
+    }
   }
 
 }
