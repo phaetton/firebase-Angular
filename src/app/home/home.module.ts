b758cbf273cb9649ae713a7197d6fccc8d425f6f
phaetton <31552767+phaetton@users.noreply.github.com>
2021-06-02 09:13:41 -0600

    colocando navbar en el principal

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { NavbarComponent } from '../shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    NavbarComponent
+    HomeComponent
   ],
   imports: [
     CommonModule,
